d438705dbcfa6bfa9af07e5a05034e566bb9bd0b
s-hassanin <82418172+s-hassanin@users.noreply.github.com>
2023-06-26 14:19:51 -0400

    Update server.js

--- cards/server.js
+++ cards/server.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.set('port', (process.env.PORT || 5000));
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -112,10 +114,31 @@ var cardList =
  'Babe Ruth'
 ];
 
-const url = 'mongodb+srv://COP4331:COP4331C@cluster0.5gewmlg.mongodb.net/?retryWrites=true&w=majority';
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production')
+{
+ // Set static folder
+ app.use(express.static('frontend/build'));
+ app.get('*', (req, res) =>
+{
+ res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+ });
+}
+
+
+require('dotenv').config();
+
 const MongoClient = require("mongodb").MongoClient;
+// const url = 'mongodb+srv://COP4331:COP4331C@cluster0.5gewmlg.mongodb.net/?retryWrites=true&w=majority';
+
+const url = process.env.MONGODB_URI;
+console.log(url);
+
+
 const client = new MongoClient(url);
+
 client.connect(console.log("mongodb connected"));
+
 
 app.post('/api/addcard', async (req, res, next) =>
 {
